781eb11785898c91c8dff6d3441c394e16aa9f8b
Nidhish Srivastava <nidhish0430@gmail.com>
2023-09-21 23:28:08 +0530

    .

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-// import app from "./index";
-// const port = process.env.PORT || 3000
-// import { connectmongodb } from "./mongodb/connect";
+import app from "./index";
+const port = process.env.PORT || 3000
+import { connectmongodb } from "./mongodb/connect";
 
-// const start = async() =>{
-//     connectmongodb()
-//     app.listen(port,()=>{
-//         console.log(`Server is listening at port ${port}`);
-//     })
-// }
+const start = async() =>{
+    connectmongodb()
+    app.listen(port,()=>{
+        console.log(`Server is listening at port ${port}`);
+    })
+}
 
-// start()
+start()

7c165a7ba0e6fedbcd86d69d0c8e978422810d02
Nidhish Srivastava <nidhish0430@gmail.com>
2023-09-21 23:24:23 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,19 @@ import cors from 'cors'
 import postRouter from './routes/post.route'
 import authRouter from './routes/auth.route'
 const app = express()
+const port = process.env.PORT || 3000
+import { connectmongodb } from "./mongodb/connect";
+
+const start = async() =>{
+    connectmongodb()
+    app.listen(port,()=>{
+        console.log(`Server is listening at port ${port}`);
+    })
+}
+
+start()
+
+
 
 app.get('/',(req:Request,res:Response)=>{
     res.send(`Welcome to Interview Tracker`)
@@ -12,4 +25,4 @@ app.use(express.json())
 app.use('/post',postRouter)
 app.use('/auth',authRouter)
 
-export default app  
+// export default app  

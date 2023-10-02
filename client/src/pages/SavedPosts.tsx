287947a8dc9c5874ff87f210c454f830a4ce9eb5
Nidhish Srivastava <nidhish0430@gmail.com>
2023-10-02 23:28:02 +0530

    .

--- client/src/pages/SavedPosts.tsx
+++ client/src/pages/SavedPosts.tsx
@@ -1,37 +1,41 @@
-import {useState,useEffect} from 'react'
-import { useTrackerContext } from '../context/context'
-import PostCard from '../components/PostCard'
-import { Link } from 'react-router-dom'
-import { FormData } from './Create'
+import { useState, useEffect } from "react";
+import { useTrackerContext } from "../context/context";
+import PostCard from "../components/PostCard";
+import { Link } from "react-router-dom";
+import { FormData } from "./Create";
 
 const SavedPosts = () => {
-    const {loggedInUser} = useTrackerContext()
-    const [savedPosts,setSavedPosts] = useState<FormData[]>([])
-    const fetchSavePosts = async() =>{
-        const response = await fetch(`http://localhost:3000/post/savedPosts/${loggedInUser?.id}`,{
-            method : "GET",
-            headers : {
-                Authorization : "Bearer " + localStorage.getItem("token")
-            }
-        })
-        const data = await response.json()
-        setSavedPosts(data)
-    }
+  const { loggedInUser } = useTrackerContext();
+  const [savedPosts, setSavedPosts] = useState<FormData[]>([]);
+  const fetchSavePosts = async () => {
+    const response = await fetch(
+      `http://localhost:3000/post/savedPosts/${loggedInUser?.id}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    );
+    const data = await response.json();
+    setSavedPosts(data);
+  };
 
-    useEffect(()=>{
-        fetchSavePosts()
-    },[])
+  useEffect(() => {
+    fetchSavePosts();
+  }, []);
   return (
     <>
-    <main className='post-container'>
-        {savedPosts.map((e,i)=>(
-    <Link to={`/${e?.username}/${e._id}`}>
-            <PostCard post={e} key={i} show = {true}/>
-        </Link>
+    <h3 style={{padding : "2rem"}}>{savedPosts.length==0 && "No posts saved !!!"}</h3>
+      <main className="post-container">
+        {savedPosts.map((e, i) => (
+          <Link to={`/${e?.username}/${e._id}`}>
+            <PostCard post={e} key={i} show={true} />
+          </Link>
         ))}
-    </main>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default SavedPosts
+export default SavedPosts;

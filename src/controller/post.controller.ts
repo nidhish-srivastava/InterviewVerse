a0c1f975544632067e9c6fcd572cf77f4762deea
Nidhish Srivastava <nidhish0430@gmail.com>
2023-10-18 00:53:47 +0530

    .

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Auth, Post } from "../mongodb/model";
+import { connectmongodb } from "mongodb/connect";
 
 interface ReqQuery {
   topic?: string; // Assuming 'topic' is a string in req.query
@@ -19,6 +20,7 @@ const cleanInput = (input: string) => {
 };
 
 export const getAll = async (req: Request, res: Response) => {
+  connectmongodb()
   const { topic, username } = req.query as ReqQuery;
   const queryObject: Record<string, any> = {}; //* wierd ts stuf :))
 
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import postRouter from './routes/post.route'
 import authRouter from './routes/auth.route'
+import { connectmongodb } from "./mongodb/connect";
+const port = process.env.PORT || 3000
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+const start = async() =>{
+    connectmongodb()
+    app.listen(port,()=>{
+        console.log(`Server is listening at port ${port}`);
+    })
+}
+
+start()
 
 app.get('/',(req:Request,res:Response)=>{
     res.send(`Welcome to Interview Tracker`)
 })
-app.use(cors())
-app.use(express.json())
 app.use('/post',postRouter)
 app.use('/auth',authRouter)
 

460ab5bcd18d5809d274ed5284592147599c8b97
Nidhish Srivastava <nidhish0430@gmail.com>
2023-12-09 12:55:21 +0530

    fixed update and delete btn ui

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <nav className="nav">
         <div>
           <Link className="home" to="/">Home</Link>
-          <Link className = "interview-track" to="/interview-tracks">Interview Tracks</Link>
+          <Link className = "interview-track" to="/interview-tracks">Interview&nbsp;Tracks</Link>
         </div>
         <div>
           {loggedInUser?.username?.length ?? 0 > 1 ? (

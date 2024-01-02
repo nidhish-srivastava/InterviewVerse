fe11978763ac0befe5154f6ec006012360d02e03
Nidhish Srivastava <nidhish0430@gmail.com>
2024-01-03 01:25:42 +0530

    .

--- client/src/pages/SinglePost.tsx
+++ client/src/pages/SinglePost.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect, Fragment } from "react";
 import { FormData } from "./Create";
 import { singlePostPromise } from "./MySinglePost";

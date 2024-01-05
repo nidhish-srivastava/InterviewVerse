2525073152d376690d604cb58d25dd488cef6107
Nidhish Srivastava <nidhish0430@gmail.com>
2024-01-06 01:09:14 +0530

    save reading list modal client fix

--- client/src/components/FullSinglePost.tsx
+++ client/src/components/FullSinglePost.tsx
@@ -17,9 +17,10 @@ const FullSinglePost = ({ singlePostObj, show }: FullSinglePost) => {
   const { loggedInUser } = useTrackerContext();
   const { isAuthenticated } = useTrackerContext();
   const [present, setPresent] = useState(false);
-  const [showModal,setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   const modalRef = useRef<HTMLDivElement | null>(null);
-  const [check,setCheck] = useState(false)
+  const [check, setCheck] = useState(false);
+  // const [existingLists,setExistingLists] = useState([])
 
   const removeSavedPost = async () => {
     const response = await fetch(
@@ -33,7 +34,7 @@ const FullSinglePost = ({ singlePostObj, show }: FullSinglePost) => {
     );
     if (response.status == 200) {
       toast.success("Removed Successfully");
-      setPresent(false)
+      setPresent(false);
       // setTimeout(() => {
       //   navigate(`/saved-posts/${loggedInUser?.username}`);
       // }, 1000);
@@ -46,8 +47,7 @@ const FullSinglePost = ({ singlePostObj, show }: FullSinglePost) => {
       userId: loggedInUser?.id,
     };
     if (isAuthenticated) {
-      setCheck(true)
-      openModal()
+      setCheck(true);
       await fetch(`${url}/post/savedPosts`, {
         method: "POST",
         headers: {
@@ -57,55 +57,87 @@ const FullSinglePost = ({ singlePostObj, show }: FullSinglePost) => {
         body: JSON.stringify(formData),
       });
       toast.success("Saved Successfully");
-      setPresent(true)
+      setPresent(true);
     } else toast.error("Please Login to save");
   };
 
-  const checkIfSavedPromise = async (): Promise<any> => {
-    const response = await fetch(
-      `${url}/post/savedPosts/check/${id}/${loggedInUser?.id}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
+  useEffect(() => {
+    const checkIfSaved = async () => {
+      const response = await fetch(
+        `${url}/post/savedPosts/check/${id}/${loggedInUser?.id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      // return response.text();
+      const data = await response.text();
+      if (data == "true") {
+        setPresent(true);
+        setCheck(true);
+      } else {
+        setCheck(false);
+        setPresent(false);
       }
-    );
-    return response.text();
+    };
+
+    checkIfSaved();
+  }, [isAuthenticated]);
+
+  const toggleModal = () => {
+    // Logic for saving the post(post should be saved when user opens the modal)
+    if (!showModal) {
+      setShowModal(true);
+      if (!present) {
+        // fetchReadingLists()
+        savePostHandler();
+      }
+    }
+    if (showModal) {
+      setShowModal(false);
+    }
   };
 
-  useEffect(() => {
-    const checkHandler = async () => {
-      const check = await checkIfSavedPromise();
-      if (check == "true") setPresent(true);
-      else setPresent(false);
-    };
-    if (isAuthenticated) {
-      checkHandler();
+  const handleCheckboxChange = () => {
+    setCheck((prev) => !prev);
+    if (!check) {
+      savePostHandler();
+      setCheck(true);
     }
-  }, []);
+    if (check) {
+      removeSavedPost();
+      setCheck(false);
+    }
+  };
 
-  const openModal = ()=>{
-    setShowModal(e=>!e)
-  }
+  // const fetchReadingLists = async()=>{
+  //   const response = await fetch(`${url}/readingList/fetchAll/${loggedInUser?.id}`)
+  //   const data = await response.json()
+  //   setExistingLists(data)
+  // }
 
   useEffect(() => {
-    const handleOutsideClick = (event : MouseEvent) => {
+    const handleOutsideClick = (event: MouseEvent) => {
       // Check if the clicked element is outside the modal
-      if (modalRef.current && !modalRef?.current?.contains(event.target as Node)) {
+      if (
+        modalRef.current &&
+        !modalRef?.current?.contains(event.target as Node)
+      ) {
         // Close the modal
         setShowModal(false);
       }
     };
 
     // Attach the event listener to the document body
-    document.body.addEventListener('click', handleOutsideClick);
+    document.body.addEventListener("click", handleOutsideClick);
 
     // Cleanup the event listener when the component is unmounted
     return () => {
-      document.body.removeEventListener('click', handleOutsideClick);
+      document.body.removeEventListener("click", handleOutsideClick);
     };
-  }, [])
+  }, []);
 
   return (
     <>
@@ -114,48 +146,67 @@ const FullSinglePost = ({ singlePostObj, show }: FullSinglePost) => {
         <h1>{singlePostObj?.topic}</h1>
         <span className="desc">{singlePostObj?.desc}</span>
         <br />
-        <div className="full-post-detail-bar">
-          <div className="dp-wrapper-profile-full-post">
-            <div className="dp-wrapper-fullpost">
-              <img src={defaultDp} alt="" loading="lazy" />
-            </div>
-            <Link to={`/${singlePostObj?.username}`}>
-              <span>{`${singlePostObj?.username}`}</span>
-            </Link>
+        <div className="dp-wrapper-profile-full-post">
+          <div className="dp-wrapper-fullpost">
+            <img src={defaultDp} alt="" loading="lazy" />
+          </div>
+          <Link to={`/${singlePostObj?.username}`}>
+            <span>{`${singlePostObj?.username}`}</span>
+          </Link>
+          &nbsp; &nbsp;
+          <div style={{ fontSize: "15px" }}>
+            {dateFormatter(singlePostObj?.createdAt as Date | number | string)}
             &nbsp; &nbsp;
-            <div style={{ fontSize: "15px" }}>
-              {dateFormatter(
-                singlePostObj?.createdAt as Date | number | string
-              )}
-              &nbsp; &nbsp;
-              {timeToReadPost(singlePostObj?.details)} min read
-            </div>
-            {
-              singlePostObj?.username != loggedInUser?.username &&
+            {timeToReadPost(singlePostObj?.details)} min read
+          </div>
+          {singlePostObj?.username != loggedInUser?.username && (
             <div className="save-icon-container" ref={modalRef}>
-              {showModal && <div className="modal-save-btn">
+              {showModal && (
+                <div className="modal-save-btn">
+                  <div className="list-bar">
+                    <input
+                      type="checkbox"
+                      id="reading-list"
+                      name="reading-list"
+                      checked={check}
+                      onChange={handleCheckboxChange}
+                    />
+                    <label htmlFor="reading-list">Reading List</label>
+                    <span>
+                      <i className="fa-solid fa-lock"></i>
+                    </span>
+                  </div>
+                  {/* {
+                  existingLists.map((e:any)=>(
                 <div className="list-bar">
-                  <input type="checkbox" checked={check}/>
-                <h4>Reading List</h4>
+                  <input type="checkbox"/>
+                <h4>{e?.name}</h4>
                 <span>
+                  {e?.visibilty!="public" &&
                 <i className="fa-solid fa-lock"></i>
-                </span>
+              }
+              </span>
                 </div>
-
-              </div> }
+                  ))
+                } */}
+                  <div
+                    style={{ marginTop: "1.3rem", marginBottom: ".7rem" }}
+                  ></div>
+                  <hr style={{ opacity: ".5" }} />
+                  <span>Create new list</span>
+                </div>
+              )}
               {present ? (
-                <div className="saved-icon" onClick={removeSavedPost}>
+                <div className="saved-icon" onClick={toggleModal}>
                   <img src={savedIcon} loading="lazy" alt="" />
                 </div>
               ) : (
-                <div className="save-icon" onClick={savePostHandler}>
+                <div className="save-icon" onClick={toggleModal}>
                   <img src={saveIcon} alt="" />
                 </div>
               )}
             </div>
-            }
-
-          </div>
+          )}
         </div>
         <span>
           <hr />
@@ -174,3 +225,9 @@ const FullSinglePost = ({ singlePostObj, show }: FullSinglePost) => {
 };
 
 export default FullSinglePost;
+
+//* I need to create an api which checks wether this post is present in all the lists or not which is quite complex
+
+// const CreateNewListModal = () =>{
+
+// }

df0673467a3b2210bb679da37fcca2d64139ded5
Nidhish Srivastava <nidhish0430@gmail.com>
2024-04-14 17:59:59 +0530

    .

--- client/src/components/Landing/MainHeroImage.tsx
+++ client/src/components/Landing/MainHeroImage.tsx
@@ -1,11 +1,10 @@
-import heroimg from "../../assets/hero.jpg"
 
 const MainHeroImage = () => {
   return (
     <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
-        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src={heroimg}
+        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full"
+        src={"/hero.jpg"}
         alt="happy team image"
         loading="lazy"
       />

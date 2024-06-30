978613ace1ff3c7320fc0611fe23106240e92b64
Nidhish Srivastava <nidhish0430@gmail.com>
2024-06-30 23:17:27 +0530

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 3000
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: 'https://interview-verse.vercel.app',
+}))
 app.use(express.json())
 
 

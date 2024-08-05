6d7ca9bd4a37d9cbdfc6c16499ebef785d887f1a
Nidhish Srivastava <nidhish0430@gmail.com>
2024-08-06 01:42:12 +0530

    added env variables for s3 config

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -6,8 +6,8 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const randomBytes = promisify(crypto.randomBytes);
-const region = "ap-south-1";
-const bucketName = "interviewverse-s3bucket";
+const region = process.env.region;
+const bucketName = process.env.bucketname;
 const accessKeyId = process.env.accessKeyId || "";
 const secretAccessKey = process.env.secretAccessKey || "";
 

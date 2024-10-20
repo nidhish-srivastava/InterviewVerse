c8c9f5ca9dfadd551b4b03983bd31c3bc2ecded5
Nidhish Srivastava <nidhish0430@gmail.com>
2024-10-20 23:47:39 +0530

    fix : navbar dp not getting displayed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
   },
   {
     path : "/search",
-    element : <Search/>
+    element : <CheckAuthentication><Search/></CheckAuthentication>
   },
   {
     path: "/:id/:id",
